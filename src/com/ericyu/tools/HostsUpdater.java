89fd91bae576953c1b3841ff9239270b487ec3f4
eric yu <yulin.jay@gmail.com>
2015-03-14 21:51:56 +0800

    use pattern

--- src/com/ericyu/tools/HostsUpdater.java
+++ src/com/ericyu/tools/HostsUpdater.java
@@ -7,6 +7,8 @@ import org.jsoup.select.Elements;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by 麟 on 2015/2/27.
@@ -14,13 +16,18 @@ import java.net.URL;
 public class HostsUpdater
 {
     private static String url_hosts = "http://www.360kb.com/kb/2_122.html";
-
+    //private static String pattern = "/^(([1-9]|([1-9]\\d)|(1\\d\\d)|(2([0-4]\\d|5[0-5])))\\.)((d|([1-9]\\d)|(1\\d\\d)|(2([0-4]\\d|5[0-5])))\\.){2}([1-9]|([1-9]\\d)|(1\\d\\d)|(2([0-4]\\d|5[0-5])))$/";
+    private static String pattern = "((2[0-4]\\d|25[0-5]|1?\\d?\\d)\\.){3}(2[0-4]\\d|25[0-5]|1?\\d?\\d)";
     public static void main(String[] args)
     {
         try
         {
             Document doc = Jsoup.parse(new URL(url_hosts).openConnection().getInputStream(), "UTF-8", url_hosts);
             String addresses = getAddressFromDocument(doc);
+            if(addresses == null)
+            {
+                return;
+            }
             UpdateHosts(addresses);
 
         } catch (IOException e)
@@ -57,14 +64,27 @@ public class HostsUpdater
     private static String getAddressFromDocument(Document doc)
     {
         //Elements eles = doc.getElementsByTag("pre");
-        Elements eles = doc.getElementsContainingText("#google hosts");
+        Elements eles = doc.getElementsContainingText("www.google.com");
         if(eles.size()==0)
         {
             System.out.println("can not find addresses.( " + eles.size()+" )" );
             return null;
         }
         String ret = eles.last().toString();
-        ret= ret.substring(ret.indexOf("#google hosts"));
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(ret);
+        String temp = null;
+        if(m.find())
+        {
+            temp = m.group();
+        }
+        if(temp == null)
+        {
+            System.out.println("can not find ip address");
+            return null;
+        }
+
+        ret= ret.substring(ret.indexOf(temp));
         ret = ret.replace("<br>", " ");
         ret = ret.replace("&nbsp;", " ");
 
